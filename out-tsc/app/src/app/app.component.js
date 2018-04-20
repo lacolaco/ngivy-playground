c361141fa5001c303e62f5df5923f07fa394d84f
Suguru Inatomi <suguru.inatomi@gmail.com>
2018-04-20 23:05:34 +0900

    update

--- out-tsc/app/src/app/app.component.js
+++ out-tsc/app/src/app/app.component.js
@@ -9,12 +9,11 @@ AppComponent.ngComponentDef = i0.ɵdefineComponent({
   factory: function AppComponent_Factory() {
     return new AppComponent();
   },
-  template: function AppComponent_Template(ctx, cm) {
-    if (cm) {
+  template: function AppComponent_Template(rf, ctx) {
+    if (rf & 1) {
       i0.ɵE(0, 'app-greeting', _c0);
       i0.ɵe();
     }
-    i0.ɵp(0, 'name', i0.ɵb('World'));
   },
   directives: [i1.GreetingComponent]
 });
--- out-tsc/app/src/app/greeting/greeting.component.js
+++ out-tsc/app/src/app/greeting/greeting.component.js
@@ -15,11 +15,13 @@ GreetingComponent.ngComponentDef = i0.ɵdefineComponent({
   factory: function GreetingComponent_Factory() {
     return new GreetingComponent();
   },
-  template: function GreetingComponent_Template(ctx, cm) {
-    if (cm) {
+  template: function GreetingComponent_Template(rf, ctx) {
+    if (rf & 1) {
       i0.ɵT(0);
     }
-    i0.ɵt(0, i0.ɵi1('Hello ', ctx.greetingName, '!'));
+    if (rf & 2) {
+      i0.ɵt(0, i0.ɵi1('Hello ', ctx.greetingName, '!'));
+    }
   },
   inputs: { greetingName: 'name' },
   features: [i0.ɵNgOnChangesFeature(GreetingComponent)]
